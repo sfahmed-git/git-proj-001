b43f1ff7ac7a05c4e855cb321018ba2f4ab6c4f4
sfahmed-git <syedfurqan.email@gmail.com>
2020-06-01 22:43:52 +0100

    Name Edited

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Its FURQAN and I have Edited <code>src/App.js</code> and saved to reload.
+          Its FURQAN AHMED and I have Edited <code>src/App.js</code> and saved to reload.
         </p>
         <a
           className="App-link"
